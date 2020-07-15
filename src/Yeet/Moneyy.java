3026589926b020fcf34e1a0a596843cd7bad4cb3
Jarrett <64062473+jarrettdev@users.noreply.github.com>
2020-07-15 15:21:20 -0500

    Update Moneyy.java

--- src/Yeet/Moneyy.java
+++ src/Yeet/Moneyy.java
@@ -13,54 +13,10 @@ import java.util.TimerTask;
 public class Moneyy {
 
     public static void main(String args[])throws Exception {
-        //Bing b1 = new Bing();
-        //b1.loadRobinhood();
-        //Youtube y1 = new Youtube();
-        //y1.startYoutube();
-        //y1.jSoupShit("https://www.yahoo.com");
-        //y1.hunt();
+
         Ignition i1 = new Ignition();
         i1.experyTimer();
         i1.profitTimer();
-//        String abc = java.time.LocalDate.now().toString();
-//        System.out.println(abc);
-//        DateFormat dateFormatter1 = new SimpleDateFormat("HH:mm:ss");
-//        Date date = dateFormatter1.parse("15:53:45");
-//        System.out.println(date.toString());
-        //Profit p1 = new Profit();
-        //p1.loadRobinhood();
-
-        //Webby w1 = new Webby();
-        //w1.initBlacklist();
-        //System.out.println(w1.blacklist);
-        //new Expery().start();
-        //new Investopedia().start();
-        //new StockListy().start();
-        //new Instagram().login();
-        //new Instagram().checkBio("https://www.instagram.com/era.cobani/");
-        //new Investopedia().makeRobinhoodPurchase("nugt",1);
-        //new Mock().open();
-        //new Webby().start();
-        //new Mock().robby("FT");
-        //new Mock().newReddit("https://www.reddit.com/r/wallstreetbets/comments/c1m88c/daily_discussion_thread_june_17_2019/?sort=confidence");
-       // Bing g1 = new Bing();
-        //g1.setHotStock("fsadfssdfs");
-        //g1.login();
-        //try{
-
-        //g1.findStock();
-        //}catch (Exception e){}
-        //g1.setHotStock("SNAP");
-        //g1.googleSearch();
-        //g1.findStock();
-        //g1.tester();
-        //BigCharts b1 = new BigCharts();
-        //b1.setHotStock("AMZN");
-//        b1.login();
-        //try{
-        //b1.findStock();
-        //}catch (Exception e){}
-
     }
 
     
